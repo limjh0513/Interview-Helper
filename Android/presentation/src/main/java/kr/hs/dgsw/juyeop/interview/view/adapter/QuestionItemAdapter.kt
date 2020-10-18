175b3930c0c4afbae25c0bcace671d01b4a7e9d6
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-10-18 19:19:49 +0900

    [ADD] QuestionFragment
    QuestionFragment 클래스의 디자인을 구현했습니다.

---
+++ Android/presentation/src/main/java/kr/hs/dgsw/juyeop/interview/view/adapter/QuestionItemAdapter.kt
@@ -0,0 +1,42 @@
+package kr.hs.dgsw.juyeop.interview.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import kr.hs.dgsw.juyeop.domain.entity.Question
+import kr.hs.dgsw.juyeop.interview.R
+import kr.hs.dgsw.juyeop.interview.databinding.ItemQuestionBinding
+import kr.hs.dgsw.juyeop.interview.viewmodel.adapter.QuestionItemViewModel
+
+class QuestionItemAdapter : RecyclerView.Adapter<QuestionItemAdapter.ViewHolder>() {
+
+    lateinit var questionItemList: List<Question>
+
+    fun setList(questionItemList: List<Question>) {
+        this.questionItemList = questionItemList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_question, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder) {
+            bind(questionItemList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return questionItemList.size
+    }
+
+    inner class ViewHolder(val binding: ItemQuestionBinding): RecyclerView.ViewHolder(binding.root) {
+        val viewModel = QuestionItemViewModel()
+
+        fun bind(question: Question) {
+            viewModel.bind(question)
+            binding.viewModel = viewModel
+        }
+    }
+}
---
+++ Android/presentation/src/main/java/kr/hs/dgsw/juyeop/interview/viewmodel/adapter/QuestionItemViewModel.kt
@@ -0,0 +1,11 @@
+package kr.hs.dgsw.juyeop.interview.viewmodel.adapter
+
+import kr.hs.dgsw.juyeop.domain.entity.Question
+import kr.hs.dgsw.juyeop.interview.base.viewmodel.BaseViewModel
+
+class QuestionItemViewModel : BaseViewModel() {
+
+    fun bind(question: Question) {
+
+    }
+}
